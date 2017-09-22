d26375673b920d34a2f2ec17322f761704fa3a27
zycLuke <605449552@qq.com>
2017-09-22 10:27:38 +0800

    4

--- A/A/AViewController.m
+++ A/A/AViewController.m
@@ -37,7 +37,7 @@
 #pragma mark - event response
 - (void)didTappedPushBViewControllerButton:(UIButton *)button
 {
-    UIViewController *viewController = [[CTMediator sharedInstance] B_viewControllerWithContentText:@"hello"];
+    UIViewController *viewController = [[CTMediator sharedInstance] B_viewControllerWithContentText:@"hello,it's me!"];
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
